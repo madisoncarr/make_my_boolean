0962c743cf32710e6c30aa5eac6aadda8084fa1f
madisoncarr <madison.carr@utexas.edu>
2018-11-04 01:05:52 -0500

    additional css styling

--- src/boolean_display.js
+++ src/boolean_display.js
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import "./styles/boolean_display.css";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
-let BooleanDisplay = props => {
-  return (
-    <div className="full-boolean-display">
-      <div className="boolean-instructions">
-        <p>Here is your Boolean Search:</p>
+class BooleanDisplay extends Component {
+  state = {}
+
+  componentDidUpdate(prevProps) {
+    // Typical usage (don't forget to compare props):
+    if (this.props.passBoolean !== prevProps.passBoolean) {
+      this.setState({ copied: false });
+    }
+  }
+
+  render() {
+    return (
+      <div className="full-boolean-display">
+        <div className="boolean-instructions">
+          <p>Here is your Boolean Search:</p>
+        </div>
+        <div className="panel panel-info" type="panel" id="panel-info">
+          <p>{this.props.passBoolean}</p>
+        </div>
+        <CopyToClipboard text={this.props.passBoolean} onCopy={() => this.setState({ copied: true })}>
+          <button className="btn btn-info">{!this.state.copied ? "Copy!" : "Copied!"}</button>
+        </CopyToClipboard>
       </div>
-      <div className="panel panel-info" type="panel" id="panel-info">
-        <p>{props.passBoolean}</p>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default BooleanDisplay;
